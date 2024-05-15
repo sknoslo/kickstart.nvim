664d2a0067511704ebdd959d1e54bceac6dfe633
Steffen Olson <solson@relias.com>
2024-05-15 19:42:56 -0400

    add oil

--- init.lua
+++ init.lua
@@ -169,6 +169,16 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup()
+
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
